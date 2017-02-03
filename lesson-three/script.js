3ed582cee5c4950d925c8a98982cb0fedbec0fb7
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-02-03 15:28:41 -0800

    Collision detection lesson finished

---
+++ lesson-three/script.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+  if(y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+    dy = -dy;
+  }
+  if(x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
+    dx = -dx;
+  }
+}
+
+setInterval(draw, 10);
