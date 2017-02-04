8739ff73d4c3df8342512dc8128db8e569d9557a
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-02-04 11:01:54 -0800

    Episode 6 and 7

--- lesson-six/script.js
+++ lesson-six/script.js
@@ -10,6 +10,21 @@ var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(c=0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(r=0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -22,12 +37,27 @@ function keyDownHandler(e) {
     leftPressed = true;
   }
 }
+
 function keyUpHandler(e) {
   if(e.keyCode == 39) {
     rightPressed = false;
   }
   else if(e.keyCode == 37) {
     leftPressed = false;
+  }
+}
+
+function collisionDetection() {
+  for(c=0; c < brickColumnCount; c++) {
+    for(r=0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
   }
 }
 
@@ -38,6 +68,7 @@ function drawBall() {
   ctx.fill();
   ctx.closePath();
 }
+
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -46,10 +77,30 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawBricks() {
+  for(c=0; c < brickColumnCount; c++) {
+    for(r=0; r < brickRowCount; r++) {
+      if(bricks[c][r].status == 1) {
+        var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+        var brickY = (r*(brickWidth+brickPadding))+brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawBricks();
   drawPaddle();
+  collisionDetection();
 
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
