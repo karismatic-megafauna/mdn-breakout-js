f41ec21df16884a0f547a326d29526a6e6c96813
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-02-04 11:22:30 -0800

    Draw score and win condition

--- lesson-six/script.js
+++ lesson-six/script.js
@@ -17,6 +17,7 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+var score = 0;
 
 var bricks = [];
 for(c=0; c < brickColumnCount; c++) {
@@ -55,10 +56,21 @@ function collisionDetection() {
         if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
           dy = -dy;
           b.status = 0;
+          score++;
+          if(score == brickRowCount*brickColumnCount) {
+            alert("YOU WIN, CONGRATZ!");
+            document.location.reload();
+          }
         }
       }
     }
   }
+}
+
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
 }
 
 function drawBall() {
@@ -100,6 +112,7 @@ function draw() {
   drawBall();
   drawBricks();
   drawPaddle();
+  drawScore();
   collisionDetection();
 
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
